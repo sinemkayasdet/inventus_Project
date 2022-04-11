8cedd3a83e5683b4456436c7a04c8d66f8525480
sinem <sinem.kaya2018gmail.com>
2022-04-11 08:32:21 -0500

    LoginPage  is created

--- src/test/java/com/inventus/page/LoginPage.java
+++ src/test/java/com/inventus/page/LoginPage.java
@@ -1,5 +1,44 @@
 package com.inventus.page;
 
+import com.inventus.utilities.utilities.ConfigurationReader;
+import com.inventus.utilities.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
-    //lkdfd;lhghjgbdfbg
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//span[.='Hoşgeldin,']")
+    public WebElement enterLoginPageButton;
+
+    @FindBy(xpath = "//span[.='veya']")
+    public WebElement kayitOlButton;
+
+
+    @FindBy(id = "edUserName")
+    public WebElement userName;
+
+    @FindBy(id = "edPassword")
+    public WebElement passWord;
+
+    @FindBy(xpath = "//a[.='Giriş Yap']")
+    public WebElement loginButton;
+
+    public  void Login(){
+
+       userName.sendKeys(ConfigurationReader.getProperty("username"));
+       passWord.sendKeys(ConfigurationReader.getProperty("password"));
+
+    }
+
+    public void LoginBy(String username,String password){
+    userName.sendKeys(username);
+    passWord.sendKeys(password);
+
+    }
+
+
 }
